11cf8f01d00a156a5d04900bcad44c09a6086f56
AdriLeon <51243138+AdriLeon@users.noreply.github.com>
2022-01-20 18:42:44 +0000

    doing formulary

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import web
 
 urls = (
-    '/(.*)', 'hello'
+    '/', 'Index'
 )
 app = web.application(urls, globals())
+render = web.template.render('templates')
 
-class hello:
-    def GET(self, name):
-        if not name:
-            name = 'World'
-        return 'Hello, ' + name + '!'
+
+class Index:
+    def GET(self):
+      return render.index()
+    def POST(self):
+      formulario = web.input()
+      return formulario.nombre
 
 if __name__ == "__main__":
     app.run()
